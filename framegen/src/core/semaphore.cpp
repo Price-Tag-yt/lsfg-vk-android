3b63970461df13985ed2cea08b35b74729b1e114
Llenoire <alpineii.inc@gmail.com>
2026-09-20 10:22:41 +0300

    Replace timeline semaphore creation with Vulkan 1.1 binary fallback

--- framegen/src/core/semaphore.cpp
+++ framegen/src/core/semaphore.cpp
@@ -5,106 +5,79 @@
 #include "core/device.hpp"
 #include "common/exception.hpp"
 
-#include <optional>
 #include <cstdint>
 #include <memory>
+#include <optional>
 #include <stdexcept>
 
 using namespace LSFG::Core;
 
 Semaphore::Semaphore(const Core::Device& device, std::optional<uint32_t> initial) {
-    // create semaphore
-    const VkSemaphoreTypeCreateInfo typeInfo{
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_TYPE_CREATE_INFO,
-        .semaphoreType = VK_SEMAPHORE_TYPE_TIMELINE,
-        .initialValue = initial.value_or(0)
-    };
+    // The Vulkan 1.1 baseline uses binary semaphores. A timeline semaphore
+    // cannot be emulated by VkSemaphore; callers must use a Fence instead.
+    if (initial.has_value())
+        throw std::logic_error("Timeline semaphores are unavailable on this device");
+
     const VkSemaphoreCreateInfo desc{
         .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-        .pNext = initial.has_value() ? &typeInfo : nullptr,
     };
     VkSemaphore semaphoreHandle{};
-    auto res = vkCreateSemaphore(device.handle(), &desc, nullptr, &semaphoreHandle);
+    const auto res = vkCreateSemaphore(device.handle(), &desc, nullptr,
+        &semaphoreHandle);
     if (res != VK_SUCCESS || semaphoreHandle == VK_NULL_HANDLE)
         throw LSFG::vulkan_error(res, "Unable to create semaphore");
 
-    // store semaphore in shared ptr
-    this->isTimeline = initial.has_value();
+    this->isTimeline = false;
     this->semaphore = std::shared_ptr<VkSemaphore>(
         new VkSemaphore(semaphoreHandle),
-        [dev = device.handle()](VkSemaphore* semaphoreHandle) {
-            vkDestroySemaphore(dev, *semaphoreHandle, nullptr);
-        }
-    );
+        [dev = device.handle()](VkSemaphore* semaphore) {
+            vkDestroySemaphore(dev, *semaphore, nullptr);
+        });
 }
 
 Semaphore::Semaphore(const Core::Device& device, int fd) {
-    // create semaphore
     const VkExportSemaphoreCreateInfo exportInfo{
         .sType = VK_STRUCTURE_TYPE_EXPORT_SEMAPHORE_CREATE_INFO,
-        .handleTypes = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_OPAQUE_FD_BIT
+        .handleTypes = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_OPAQUE_FD_BIT,
     };
     const VkSemaphoreCreateInfo desc{
         .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-        .pNext = &exportInfo
+        .pNext = &exportInfo,
     };
     VkSemaphore semaphoreHandle{};
-    auto res = vkCreateSemaphore(device.handle(), &desc, nullptr, &semaphoreHandle);
+    auto res = vkCreateSemaphore(device.handle(), &desc, nullptr,
+        &semaphoreHandle);
     if (res != VK_SUCCESS || semaphoreHandle == VK_NULL_HANDLE)
         throw LSFG::vulkan_error(res, "Unable to create semaphore");
 
-    // import semaphore from fd
-    auto vkImportSemaphoreFdKHR = reinterpret_cast<PFN_vkImportSemaphoreFdKHR>(
+    const auto importSemaphoreFd = reinterpret_cast<PFN_vkImportSemaphoreFdKHR>(
         vkGetDeviceProcAddr(device.handle(), "vkImportSemaphoreFdKHR"));
+    if (importSemaphoreFd == nullptr)
+        throw LSFG::vulkan_error(VK_ERROR_EXTENSION_NOT_PRESENT,
+            "vkImportSemaphoreFdKHR is unavailable");
 
     const VkImportSemaphoreFdInfoKHR importInfo{
         .sType = VK_STRUCTURE_TYPE_IMPORT_SEMAPHORE_FD_INFO_KHR,
         .semaphore = semaphoreHandle,
         .handleType = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_OPAQUE_FD_BIT,
-        .fd = fd // closes the fd
+        .fd = fd,
     };
-    res = vkImportSemaphoreFdKHR(device.handle(), &importInfo);
+    res = importSemaphoreFd(device.handle(), &importInfo);
     if (res != VK_SUCCESS)
         throw LSFG::vulkan_error(res, "Unable to import semaphore from fd");
 
-    // store semaphore in shared ptr
     this->isTimeline = false;
     this->semaphore = std::shared_ptr<VkSemaphore>(
         new VkSemaphore(semaphoreHandle),
-        [dev = device.handle()](VkSemaphore* semaphoreHandle) {
-            vkDestroySemaphore(dev, *semaphoreHandle, nullptr);
-        }
-    );
+        [dev = device.handle()](VkSemaphore* semaphore) {
+            vkDestroySemaphore(dev, *semaphore, nullptr);
+        });
 }
 
-void Semaphore::signal(const Core::Device& device, uint64_t value) const {
-    if (!this->isTimeline)
-        throw std::logic_error("Invalid timeline semaphore");
-
-    const VkSemaphoreSignalInfo signalInfo{
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_SIGNAL_INFO,
-        .semaphore = this->handle(),
-        .value = value
-    };
-    auto res = vkSignalSemaphore(device.handle(), &signalInfo);
-    if (res != VK_SUCCESS)
-        throw LSFG::vulkan_error(res, "Unable to signal semaphore");
+void Semaphore::signal(const Core::Device&, uint64_t) const {
+    throw std::logic_error("Timeline semaphores are unavailable on this device");
 }
 
-bool Semaphore::wait(const Core::Device& device, uint64_t value, uint64_t timeout) const {
-    if (!this->isTimeline)
-        throw std::logic_error("Invalid timeline semaphore");
-
-    VkSemaphore semaphore = this->handle();
-    const VkSemaphoreWaitInfo waitInfo{
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_WAIT_INFO,
-        .semaphoreCount = 1,
-        .pSemaphores = &semaphore,
-        .pValues = &value
-    };
-    auto res = vkWaitSemaphores(device.handle(), &waitInfo, timeout);
-    if (res != VK_SUCCESS && res != VK_TIMEOUT)
-        throw LSFG::vulkan_error(res, "Unable to wait for semaphore");
-
-    return res == VK_SUCCESS;
+bool Semaphore::wait(const Core::Device&, uint64_t, uint64_t) const {
+    throw std::logic_error("Timeline semaphores are unavailable on this device");
 }
